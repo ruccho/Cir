028f5d0f3fbe488a9cf5417794107a194bb79f90
ruccho <ruccho@setonoboru-no-MacBook-Pro.local>
2016-03-16 18:44:22 +0900

    ver0.1.0.1
    回転速度アップ
    落下中の回転防止
    stage2を追加

--- Assets/Scripts/PlayingManager.cs
+++ Assets/Scripts/PlayingManager.cs
@@ -14,6 +14,11 @@ public class PlayingManager : MonoBehaviour {
 	public float rotationPerFrame = 1;
 	float tempRotation;
 
+	bool isMoving = false;
+	float timerRotationFinished;
+	bool isRotationFinished = true;
+	public float CoolTimeSeconds;
+
 	// Use this for initialization
 	void Start () {
 		PlayerObject = (GameObject)Instantiate(PlayerPrefab, StartPos.transform.position, StartPos.transform.rotation);
@@ -24,19 +29,31 @@ public class PlayingManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		/*
-		if (System.Math.Abs(targetRotation % 360) == 0 || System.Math.Abs(targetRotation % 360) == 180) {
-			PlayerObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-			PlayerObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
-		}
-		if (System.Math.Abs(targetRotation % 360) == 90 || System.Math.Abs(targetRotation % 360) == 270){
-			PlayerObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-			PlayerObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
-		}*/
+
+
+		//落下中かをチェック
+		//VelocityのYがマイナス（落下中）の時で判定
+		isMoving = ((int)PlayerObject.GetComponent<Rigidbody2D>().velocity.y < 0);
+		//Debug.Log(PlayerObject.GetComponent<Rigidbody2D>().velocity.y.ToString());
+
 		if(turningState == 1 && targetRotation > tempRotation){
+
+			//回転中に設定
+			isRotationFinished = false;
+
+			//回転終了からの時間を記録するタイマーをリフレッシュ
+			timerRotationFinished = 0f;
+
 			//左回り設定で、左回りの余地あり
 			tempRotation += rotationPerFrame;
 		}else if(turningState == -1 && targetRotation < tempRotation){
+
+			//回転中に設定
+			isRotationFinished = false;
+
+			//回転終了からの時間を記録するタイマーをリフレッシュ
+			timerRotationFinished = 0f;
+
 			//右回り設定で、右回りの余地あり
 			tempRotation -= rotationPerFrame;
 		}else{
@@ -45,36 +62,26 @@ public class PlayingManager : MonoBehaviour {
 			tempRotation = targetRotation;
 			PlayerObject.GetComponent<Rigidbody2D>().gravityScale = 1;
 
+			//回転終了からの時間を記録するタイマーを更新
+			timerRotationFinished += Time.deltaTime;
+
+			//回転終了からの時間をチェック、クールタイムが終了していればisRotationFinishedをtrueに
+			isRotationFinished = (timerRotationFinished >= CoolTimeSeconds);
 		}
 
 		Stage.transform.eulerAngles = new Vector3(0,0,tempRotation);
 
-		/*
-		Debug.Log(isTurning.ToString());
-		if(isTurning){
-			Quaternion stageRotation = Stage.transform.rotation;
-			if(stageRotation.z < targetRotation){
-				//左回り中
-				Stage.transform.Rotate(new Vector3(0,0,stageRotation.z + rotationPerFrame));
-			}else if(stageRotation.z > targetRotation){
-				//右回り中
-				Stage.transform.Rotate(new Vector3(0,0,stageRotation.z - rotationPerFrame));
-			}else{
-				//回転終了
-				isTurning = false;
-				Player.GetComponent<Rigidbody2D>().WakeUp();
-			}
-		}*/
+		Debug.Log("回転が終了してから: " + timerRotationFinished + "秒");
 	}
 
 	public void RotateLeft(){
-		if(turningState != 0) return;//回転中の場合は無効
+		if(turningState != 0 || isMoving || !isRotationFinished) return;//回転中or落下中or回転終了からすぐの場合は無効
 		PlayerObject.GetComponent<Rigidbody2D>().gravityScale = 0;
 		turningState = 1;
 		targetRotation = tempRotation + 90;
 	}
 	public void RotateRight(){
-		if(turningState != 0) return;//回転中の場合は無効
+		if(turningState != 0 || isMoving || !isRotationFinished) return;//回転中or落下中or回転終了からすぐの場合は無効
 		PlayerObject.GetComponent<Rigidbody2D>().gravityScale = 0;
 		turningState = -1;
 		targetRotation = tempRotation - 90;
