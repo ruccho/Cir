27d4627fa45ef5d5f6c9cda189d9dda206dd4804
ruccho <ruccho@setonoboru-no-MacBook-Pro.local>
2016-04-10 13:41:40 +0900

    Crash後

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -4,14 +4,23 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    
+
     public GameObject FadeImage;
     public float FadeTimeSecond;
+    public GameObject MainCamera;
+    public GameObject TitleCanvasObject;
+    public GameObject ModeSelectCanvasObject;
+    public GameObject PlayPresetCanvasObject;
+    Canvas TitleCanvas;
+    Canvas ModeSelectCanvas;
+    Canvas PlayPresetCanvas;
 
     // Use this for initialization
     void Start()
     {
-
+        TitleCanvas = TitleCanvasObject.GetComponent<Canvas>();
+        ModeSelectCanvas = ModeSelectCanvasObject.GetComponent<Canvas>();
+        PlayPresetCanvas = PlayPresetCanvasObject.GetComponent<Canvas>();
     }
 
     // Update is called once per frame
@@ -22,17 +31,69 @@ public class TitleManager : MonoBehaviour
 
     public void ButtonStart()
     {
-        FadeImage.GetComponent<Fade>().FadeIn(0.2f);
-        Invoke("GoPlay", FadeTimeSecond);
+        MainCamera.GetComponent<Animator>().SetTrigger("Forward");
+        SetActiveScene(ModeSelectCanvas);
     }
-    
-    public void GoPlay()
+
+    void GoPlayScene()
     {
         SceneManager.LoadScene("Play");
     }
 
+    public void ButtonBack()
+    {
+        MainCamera.GetComponent<Animator>().SetTrigger("Backward");
+        if (TitleCanvas.enabled)
+        {
+            return;
+        }
+        else if (ModeSelectCanvas.enabled)
+        {
+            SetActiveScene(TitleCanvas);
+            return;
+        }
+        else if (PlayPresetCanvas.enabled)
+        {
+            SetActiveScene(ModeSelectCanvas);
+            return;
+        }
+        return;
+    }
+
+    void FadeAndGoPlay()
+    {
+        FadeImage.GetComponent<Fade>().FadeIn(FadeTimeSecond);
+        Invoke("GoPlayScene", FadeTimeSecond);
+    }
+
+    public void ButtonPlayPreset(int StageNumber)
+    {
+        //Resources/StagesからテキストファイルをロードしPlayerPrefsのCurrentStageTextにセット
+        string stageText = ((TextAsset)Resources.Load("Stages/Stage_" + StageNumber.ToString())).text;
+        PlayerPrefs.SetString("CurrentStageText", stageText);
+        FadeAndGoPlay();
+    }
+
+    public void ButtonNomalMode()
+    {
+
+    }
+
+
+    public void ButtonCreate()
+    {
+
+    }
     public void ButtonInfo()
     {
 
+    }
+
+    public void SetActiveScene(Canvas ActiveCanvas)
+    {
+        TitleCanvas.enabled = false;
+        ModeSelectCanvas.enabled = false;
+        PlayPresetCanvas.enabled = false;
+        ActiveCanvas.enabled = true;
     }
 }
